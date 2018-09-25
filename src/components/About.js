55c3d819378570a9807ed80120ddf5c31a8b5d3f
Pratap Dessai <pratap.dessai@inmobi.com>
2018-09-26 01:49:08 +0530

    working app

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from './../hoc/Rainbow';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, dolor alias libero quaerat sint rem officia esse! Consequatur dolorum aliquid nemo quibusdam, ipsam perspiciatis vel repudiandae quidem mollitia eveniet placeat.</p>
+    </div>
+  )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = (props) => {
+  
+  // setTimeout(()=> {
+  //   props.history.push('/about');
+  // }, 2000);
+
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, dolor alias libero quaerat sint rem officia esse! Consequatur dolorum aliquid nemo quibusdam, ipsam perspiciatis vel repudiandae quidem mollitia eveniet placeat.</p>
+    </div>
+  )
+}
+
+export default Contact;
